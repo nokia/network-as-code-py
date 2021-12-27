24f18057bf7db9ef75a96f735b558d5285cc42f0
Sami Lahtinen <sami.lahtinen@nokia.com>
2021-12-27 11:19:55 +0200

    Started prototyping the NetworkSlice functionality

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ drone_location.refresh()
 drone_location.latitude
 drone_location.longitude
 drone_location.altitude
+
+network_slice = nac.NetworkSlice(
+    drone,
+    index: 0,
+    qos: "best-effort",
+    bandwidth: "300 Mbps",
+    default: True
+)
+
+network_slice.destroy()
--- network_as_code/NetworkSlice.py
+++ network_as_code/NetworkSlice.py
@@ -1,4 +1,5 @@
 from .Device import Device
+from .RequestHandler import RequestHandler
 
 
 class NetworkSlice:
@@ -10,4 +11,41 @@ class NetworkSlice:
         bandwidth,
         default: bool,
     ) -> None:
-        pass
+        self.device = device
+        self.index = index
+        self.qos = qos
+        self.bandwidth = bandwidth
+        self.active = False
+
+        self._setup()
+
+    def _setup(self):
+        res = RequestHandler.instance().create_network_slice(self)
+        if res.status_code == 200:
+            self.id = res.json()
+            self.active = True
+
+    def update(
+        self,
+        index: int,
+        qos,
+        bandwidth,
+        default: bool
+    ):
+        self.index = index
+        self.qos = qos
+        self.bandwidth = bandwidth
+        self.default = default
+
+        self._update()
+
+    def _update(self):
+        res = RequestHandler.instance().update_network_slice(self)
+        if res.status_code == 200:
+            self.id = res.json()
+            self.active = True
+
+    def destroy(self):
+        res = RequestHandler.instance().delete_network_slice(self)
+        if res.status_code == 200:
+            self.active = False
