ceeff7c58362e091ee649c9dde09c21ab1ccd5ad
Garmuyev Pavel <pavel.garmuyev@nokia.com>
2021-12-27 11:51:10 +0200

    Remove unused NetworkSliceBuilder from imports of __init__.py

--- network_as_code/__init__.py
+++ network_as_code/__init__.py
@@ -1,6 +1,6 @@
 from .Device import Device
 from .DeviceLocation import DeviceLocation
-from .NetworkSlice import NetworkSlice, NetworkSliceBuilder
+from .NetworkSlice import NetworkSlice
 from .RequestHandler import RequestHandler
 
 __version__ = "0.1.0"
