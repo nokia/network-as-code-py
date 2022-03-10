fdc49b83b9d16d5c3e62dda4bceaec36693742e6
garmuyev <pavel.garmuyev@nokia.com>
2022-03-10 11:45:46 +0200

    Update and fix tests

--- tests/test_nac.py
+++ tests/test_nac.py
@@ -1,9 +1,10 @@
 import pytest
+from dateutil.parser import parse
 from hypothesis import given, settings, strategies as st, HealthCheck
 from network_as_code import NetworkProfile, Device, GeoZone
 from network_as_code.errors import GatewayConnectionError
 
-API_PATH = "https://apigee-api-test.nokia-solution.com/network-as-code"
+API_PATH = "https://apigee-api-test.nokia-solution.com/nac"
 
 
 @pytest.fixture
@@ -15,23 +16,19 @@ def test_device_init():
     test_id = "example@example.com"
     test_sdk_token = "random_api_token"
     test_device = Device(test_id, test_sdk_token)
-    assert test_device.ext_id == test_id
+    assert test_device.id == test_id
     assert test_device.sdk_token == test_sdk_token
 
 
 def test_mocked_api_connection(requests_mock, device):
-    requests_mock.get(
-        f"{API_PATH}/hello",
-        json={"service": "up"},
-    )
-
+    requests_mock.get(f"{API_PATH}/hello", json={"service": "up"})
     assert device.check_api_connection()
 
 
 @given(
     latitude=st.floats(min_value=-90, max_value=90),
     longitude=st.floats(min_value=-180, max_value=180),
-    # altitude=st.floats(min_value=0, max_value=1000),
+    elevation=st.floats(min_value=0, max_value=1000),
     # timestamp=st.floats(allow_nan=False),
 )
 @settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
@@ -39,27 +36,35 @@ def test_successful_device_location(
     requests_mock,
     device,
     latitude,
-    longitude,  # altitude, timestamp
+    longitude,
+    elevation,  # timestamp
 ):
     # Register a mocked response
+    timestamp = "2022-03-08T17:12:00Z"
     requests_mock.post(
         f"{API_PATH}/subscriber/location",
         json={
-            "latitude": latitude,
-            "longitude": longitude,
-            # "altitude": altitude,
-            # "timestamp": timestamp,
+            "eventTime": timestamp,
+            "ueId": "todd.levi@nokia.com",
+            "locationInfo": {
+                "ageOfLocationInfo": "2038",
+                "trackingAreaId": "3100120016A8",
+                "plmnId": "310012",
+                "lat": str(latitude),
+                "long": str(longitude),
+                "elev": str(elevation),
+            },
         },
     )
 
     # Get the device location
-    device_location = device.location()
+    lat, lon, elev, ts = device.location()
 
-    # Assert that the data is received and stored correctly
-    assert device_location["latitude"] == latitude
-    assert device_location["longitude"] == longitude
-    # assert device_location.altitude == altitude
-    # assert device_location.timestamp == timestamp
+    # Assert that the data is received and parsed correctly
+    assert lat == latitude
+    assert lon == longitude
+    assert elev == elevation
+    assert ts == parse(timestamp)
 
 
 def test_geozone_notification(device):
@@ -70,24 +75,14 @@ def test_geozone_notification(device):
 
 
 def test_successful_network_profile_selection(requests_mock, device):
-    requests_mock.patch(
-        f"{API_PATH}/subscriber/bandwidth",
-        text="",
-    )
+    requests_mock.patch(f"{API_PATH}/subscriber/bandwidth", text="")
     network_profile = NetworkProfile("gold")
-
     device.apply(network_profile)
-
     assert network_profile.bandwidth_profile == "gold"
 
-def test_unsuccessful_network_profile_selection(
-    requests_mock, device
-):
-    requests_mock.patch(
-        f"{API_PATH}/subscriber/bandwidth",
-        status_code=404,
-    )
 
+def test_unsuccessful_network_profile_selection(requests_mock, device):
+    requests_mock.patch(f"{API_PATH}/subscriber/bandwidth", status_code=404)
     try:
         network_profile = NetworkProfile("gold")
         device.apply(network_profile)
@@ -96,35 +91,24 @@ def test_unsuccessful_network_profile_selection(
     except GatewayConnectionError:
         assert True
 
-def test_network_profile_selection_using_setter_updates_value(
-    requests_mock, device
-):
+
+def test_network_profile_selection_using_setter_updates_value(requests_mock, device):
     network_profile = NetworkProfile("gold")
-
     network_profile.bandwidth_profile = "bronze"
-
     assert network_profile.bandwidth_profile == "bronze"
 
+
 def test_network_profile_selection_produces_correct_json_body(requests_mock):
-    device = Device(sdk_token="blah", ext_id="example@example.com")
-
-    requests_mock.patch(
-        f"{API_PATH}/subscriber/bandwidth",
-        text=_json_body_callback,
-    )
-
+    device = Device(sdk_token="blah", id="example@example.com")
+    requests_mock.patch(f"{API_PATH}/subscriber/bandwidth", text=_json_body_callback)
     network_profile = NetworkProfile("gold")
-
     device.apply(network_profile)
-
     assert network_profile.bandwidth_profile == "gold"
 
 
 def _json_body_callback(request, context):
     json_body = request.json()
-
-    assert json_body["externalid"] == "example@example.com"
+    assert json_body["id"] == "example@example.com"
     assert json_body["bandwidthID"] == "gold"
-
     context.status_code = 200
     return ""
