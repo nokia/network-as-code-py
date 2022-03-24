da88787ad2321ccc64c6b37724614a1b9d0c1c18
garmuyev <pavel.garmuyev@nokia.com>
2022-03-24 12:42:01 +0200

    Add API request debugging and Accept header to every request

--- network_as_code/RequestHandler.py
+++ network_as_code/RequestHandler.py
@@ -1,3 +1,5 @@
+import os
+import pprint
 import requests
 from .errors import GatewayConnectionError
 
@@ -8,7 +10,9 @@ if TYPE_CHECKING:  # Avoids cyclic imports for type hints
 
 
 class RequestHandler:
-    api_url = "https://apigee-api-test.nokia-solution.com/nac"
+    api_version = "v2"
+    api_url = f"https://apigee-api-test.nokia-solution.com/nac/{api_version}"
+    accept_header = "application/json"
 
     @classmethod
     def _request(cls, method: str, path: str, headers: dict, json: dict, **kwargs):
@@ -17,30 +21,33 @@ class RequestHandler:
         try:
             res = requests.request(method, url, headers=headers, json=json, **kwargs)
             res.raise_for_status()  # Raises an exception if status_code is in [400..600)
+            if os.getenv("DEBUG"):
+                print(f"{method} /{path} ({json})")
+                pprint.pprint(res.json(), width=88, compact=True, sort_dicts=False)
+            return res
         except:
             raise GatewayConnectionError("Can't connect to the backend service")
-        return res
 
     @classmethod
     def get_location(cls, device: "Device"):
-        headers = {"x-apikey": device.sdk_token}
+        headers = {"x-apikey": device.sdk_token, "Accept": cls.accept_header}
         json = {"id": device.id}
         return cls._request("POST", "/subscriber/location", headers, json)
 
     @classmethod
     def get_network_profile(cls, device: "Device", **json: dict):
-        headers = {"x-apikey": device.sdk_token}
+        headers = {"x-apikey": device.sdk_token, "Accept": cls.accept_header}
         json["id"] = device.id
         return cls._request("POST", "/subscriber/bandwidth", headers, json)
 
     @classmethod
     def set_network_profile(cls, device: "Device", **json: dict):
-        headers = {"x-apikey": device.sdk_token}
+        headers = {"x-apikey": device.sdk_token, "Accept": cls.accept_header}
         json["id"] = device.id
         return cls._request("PATCH", "/subscriber/bandwidth", headers, json)
 
     @classmethod
-    def check_api_connection(cls, device):
-        headers = {"x-apikey": device.sdk_token}
+    def check_api_connection(cls, device: "Device"):
+        headers = {"x-apikey": device.sdk_token, "Accept": cls.accept_header}
         res = cls._request("GET", "/hello", headers, None)
         return res.status_code
