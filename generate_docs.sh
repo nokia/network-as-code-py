5971f021cafe0691414c629971c2e94736e43613
garmuyev <pavel.garmuyev@nokia.com>
2022-03-29 11:54:45 +0300

    First try bash script working?

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-mkdir -p docs/
+project_wiki_url="https://gitlabe2.ext.net.nokia.com/api/v4/projects/41791/wikis"
+modules="Configuration CustomNetworkProfile DeviceLocation Device GeoZone NetworkProfile"
+base_page="Reference"
 
-poetry run pydoc-markdown -m network_as_code.Configuration --render-toc > docs/Configuration.md
-poetry run pydoc-markdown -m network_as_code.CustomNetworkProfile --render-toc > docs/CustomNetworkProfile.md
-poetry run pydoc-markdown -m network_as_code.DeviceLocation --render-toc > docs/DeviceLocation.md
-poetry run pydoc-markdown -m network_as_code.Device --render-toc > docs/Device.md
-poetry run pydoc-markdown -m network_as_code.GeoZone --render-toc > docs/GeoZone.md
-poetry run pydoc-markdown -m network_as_code.NetworkProfile --render-toc > docs/NetworkProfile.md
+for module in $modules; do
+    content="title=${base_page}/${module}&content=$(poetry run pydoc-markdown -m network_as_code.${module} --render-toc)"
+    page_url="${project_wiki_url}/${base_page}%2F${module}"
+    check_code=$(curl -s -o /dev/null -w "%{http_code}\n" --header "PRIVATE-TOKEN: $GITLAB_API_TOKEN" $page_url)
+    if [ $check_code = "200" ]; then
+        curl --request PUT --data $content --header "PRIVATE-TOKEN: $GITLAB_API_TOKEN" $page_url
+    else
+        curl --data $content --header "PRIVATE-TOKEN: $GITLAB_API_TOKEN" $project_wiki_url
+    fi
+done
