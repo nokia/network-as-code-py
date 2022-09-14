8199e300bb381e849d308b4d09941160ae7fd61e
Pavel Garmuyev <pavel.garmuyev@nokia.com>
2022-09-14 13:18:32 +0300

    Move to using HTTPX's request method & fix testuser deletion

--- network_as_code/api/client.py
+++ network_as_code/api/client.py
@@ -29,51 +29,24 @@ class APIClient(
         timeout: int = 5,
         base_url: str = None,
         testmode: bool = False,
+        **kwargs,
     ):
-        super().__init__()
+        super().__init__(**kwargs)
 
         self.timeout = timeout
         self.base_url = (
             "https://apigee-api-test.nokia-solution.com/nac/v4"
             if base_url is None
             else base_url
-        ).rstrip("/")
+        )
 
         # Set the default headers for all API requests
-        self.headers.update(
-            {
+        self.headers = {
                 "x-apikey": token,
                 "x-testmode": "true" if testmode else "false",
                 "Accept": "application/json",
                 "Content-Type": "application/json",
             }
-        )
-
-    # TODO: Don't Repeat Yourself...
-    def _get(self, path: str, **kwargs):
-        return self._request("GET", path, **kwargs)
-
-    def _post(self, path: str, **kwargs):
-        return self._request("POST", path, **kwargs)
-
-    def _put(self, path: str, **kwargs):
-        return self._request("PUT", path, **kwargs)
-
-    def _patch(self, path: str, **kwargs):
-        return self._request("PATCH", path, **kwargs)
-
-    def _delete(self, path: str, **kwargs):
-        return self._request("DELETE", path, **kwargs)
-
-    def _request(self, method: str, path: str, **kwargs):
-        url = f"{self.base_url}/{path.lstrip('/')}"
-
-        try:
-            res = self.request(method, url, timeout=self.timeout, **kwargs)
-            return res
-        except httpx.ReadTimeout as e:
-            print(f"ERROR: The API gateway did not respond in {self.timeout} seconds.")
-            sys.exit(1)  # TODO: Should this raise another error instead?
 
     def _result(self, response: httpx.Response, json=False, raw=False):
         assert not (json and raw)  # Can't have both output types selected
@@ -81,7 +54,7 @@ class APIClient(
             response.raise_for_status()
         except httpx.HTTPStatusError as e:
             # TODO: Parse errors instead of simply printing their body content
-            print("ERROR: The API gateway returned the following error:")
+            print("ERROR: The API gateway returned the following error:", end="")
             try:
                 print(JSON.dumps(response.json(), indent=2))
             except Exception:
