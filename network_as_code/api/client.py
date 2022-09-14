2bb5c4b6d9d7cc0fff49717e51b92efae083ec14
Pavel Garmuyev <pavel.garmuyev@nokia.com>
2022-09-14 10:17:12 +0300

    Replace requests with httpx, add some error handling.

--- network_as_code/api/client.py
+++ network_as_code/api/client.py
@@ -1,7 +1,7 @@
 import sys
 import json as JSON
+import httpx
 from textwrap import indent
-import requests
 from .admin import AdminAPI
 from .services import ServicesAPI
 from .subscription import SubscriptionAPI
@@ -9,7 +9,7 @@ from ..errors import APIError
 
 
 class APIClient(
-    requests.Session,
+    httpx.Client,
     AdminAPI,
     ServicesAPI,
     SubscriptionAPI,
@@ -37,7 +37,7 @@ class APIClient(
             "https://apigee-api-test.nokia-solution.com/nac/v4"
             if base_url is None
             else base_url
-        )
+        ).rstrip("/")
 
         # Set the default headers for all API requests
         self.headers.update(
@@ -67,21 +67,25 @@ class APIClient(
 
     def _request(self, method: str, path: str, **kwargs):
         url = f"{self.base_url}/{path.lstrip('/')}"
+
         try:
             res = self.request(method, url, timeout=self.timeout, **kwargs)
             return res
-        except requests.exceptions.ReadTimeout as e:
+        except httpx.ReadTimeout as e:
             print(f"ERROR: The API gateway did not respond in {self.timeout} seconds.")
             sys.exit(1)  # TODO: Should this raise another error instead?
 
-    def _result(self, response: requests.Response, json=False, raw=False):
+    def _result(self, response: httpx.Response, json=False, raw=False):
         assert not (json and raw)  # Can't have both output types selected
         try:
             response.raise_for_status()
-        except requests.HTTPError as e:
+        except httpx.HTTPStatusError as e:
             # TODO: Parse errors instead of simply printing their body content
             print("ERROR: The API gateway returned the following error:")
-            print(JSON.dumps(response.json(), indent=2))
+            try:
+                print(JSON.dumps(response.json(), indent=2))
+            except Exception:
+                print(response.text)
             sys.exit(1)  # TODO: Should this raise APIError instead?
 
         if json:
