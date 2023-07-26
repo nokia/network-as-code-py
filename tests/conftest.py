771cece9bc414ab1bd39c7ed6134c7c717fa25d0
Sami Lahtinen <sami.lahtinen@nokia.com>
2023-07-26 10:44:36 +0300

    Remove unnecessary runtime deps
    We had some leftover runtime dependencies from incubation phase, this
    cleans those up and moves some of the test deps into test dep group.
    
    Also as a bonus, we load .env on conftest, so that setting the
    NAC_TOKEN is easier.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,13 @@ import os
 
 import pytest
 
+from dotenv import load_dotenv
+
 import network_as_code as nac
 from network_as_code import NetworkAsCodeClient
 
 @pytest.fixture(scope="module")
 def client() -> NetworkAsCodeClient:
+    load_dotenv()
     token = os.environ["NAC_TOKEN"]
     return NetworkAsCodeClient(token=token)
