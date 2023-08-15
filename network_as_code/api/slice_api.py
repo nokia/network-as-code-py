884cf5496fa497176b4bacd40ec9d5e3168be493
hbirru <henok.birru@nokia.com>
2023-08-15 10:55:37 +0300

    http headers removed from each binding method

--- network_as_code/api/slice_api.py
+++ network_as_code/api/slice_api.py
@@ -5,19 +5,18 @@ from typing import Optional
 
 class SliceAPI:
     def __init__(self, base_url: str, rapid_key: str, rapid_host: str) -> None:
-        self.rapid_key = rapid_key
-        self.rapid_host = rapid_host
-        self.client = httpx.Client(base_url=base_url)   
+        self.client = httpx.Client(
+            base_url=base_url,
+            headers={
+            "content-type": "application/json",
+            "X-RapidAPI-Key": rapid_key,
+            "X-RapidAPI-Host": rapid_host
+        })   
 
 
     def create(self,body):
         response = self.client.post(
             url="/slices",
-            headers={
-                'content-type': 'application/json',
-                'X-RapidAPI-Key': self.rapid_key,
-                'X-RapidAPI-Host': self.rapid_host
-            },
             data=body
         )
 
@@ -29,10 +28,6 @@ class SliceAPI:
     def getAll(self):
         res = self.client.get(
             url="/slices",
-            headers={
-                'X-RapidAPI-Key': self.rapid_key,
-                'X-RapidAPI-Host': self.rapid_host
-            }
         )
 
         res.raise_for_status()
@@ -42,10 +37,6 @@ class SliceAPI:
     def get(self, slice_id: str):
         res = self.client.get(
             url=f"/slices/{slice_id}",
-            headers={
-                'X-RapidAPI-Key': self.rapid_key,
-                'X-RapidAPI-Host': self.rapid_host
-            }
         )
 
         res.raise_for_status()
@@ -55,10 +46,6 @@ class SliceAPI:
     def activate(self, slice_id: str):
         res = self.client.post(
             url=f"/slices/{slice_id}/activate",
-            headers={
-                'X-RapidAPI-Key': self.rapid_key,
-                'X-RapidAPI-Host': self.rapid_host
-            }
         )
 
         res.raise_for_status()
@@ -68,19 +55,11 @@ class SliceAPI:
     def deactivate(self, slice_id: str):
         return self.client.post(
             url=f"/slices/{slice_id}/deactivate",
-            headers={
-                'X-RapidAPI-Key': self.rapid_key,
-                'X-RapidAPI-Host': self.rapid_host
-            }
         )
     
     def delete(self, slice_id: str):
-        res = self.client.post(
+        res = self.client.delete(
             url=f"/slices/{slice_id}/delete",
-            headers={
-                'X-RapidAPI-Key': self.rapid_key,
-                'X-RapidAPI-Host': self.rapid_host
-            }
         )
 
         res.raise_for_status()
@@ -103,8 +82,6 @@ def delete_none(_dict):
 
 class AttachAPI:
     def __init__(self, base_url: str, rapid_key: str, rapid_host: str) -> None:
-        self.rapid_key = rapid_key
-        self.rapid_host = rapid_host
         self.client = httpx.Client(base_url=base_url, headers={"X-RapidAPI-Key": rapid_key, "X-RapidAPI-Host": rapid_host})   
 
     def attach(self, device, slice_id: str, notification_url: str, notification_auth_token: Optional[str] = None):
