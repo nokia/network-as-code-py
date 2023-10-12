061d14a58a2ad9806f9a8c9fd385ae2853b100fb
Sami Lahtinen <sami.lahtinen@nokia.com>
2023-10-12 10:44:51 +0300

    Update the Slice LCM test

--- examples/device-status.py
+++ examples/device-status.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+
+# device-status.py
 
 import network_as_code as nac
 
@@ -7,7 +7,7 @@ client = nac.NetworkAsCodeClient(
     token="<MY_TOKEN>"
 )
 
-device = client.devices.get("testuser@open5glab.net", ip="1.1.1.2")
+device = client.devices.get("device@bestcsp.net", ip="1.1.1.2")
 
 subscription = client.connectivity.subscribe(
     event_type="CONNECTIVITY",
@@ -17,17 +17,66 @@ subscription = client.connectivity.subscribe(
     notification_auth_token="my_token"
 )
 
-subscription.delete()
+# !!! CUT SNIPPET HERE !!!
+
+# status_handler.py for ROAMING_STATUS
+
+# run with: uvicorn status_handler:app
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class RoamingEventDetail(BaseModel):
+    roaming: bool | None
+    countryCode: int | None
+    countryName: List[String] | None
+
+class Event(BaseModel):
+    eventType: str
+    eventTime: str
+    eventDetail: RoamingEventDetail
+
 class Notification(BaseModel):
-    id: str
-    status: str
+    eventSubscriptionId: str
+    event: Event
 
 @app.post("/notifications")
 def receive_notification(notification: Notification):
-    if notification.status == "UNREACHABLE":
-        print("Device is offline!")
-    elif notification.status == "REACHABLE":
-        print("Device is online!")
+    if notification.event.eventDetail.roaming:
+        print("Device is roaming")
+    else:
+        print("Device is not roaming")
+
+
+# !!! CUT SNIPPET HERE !!!
+
+# status_handler.py for CONNECTIVITY
+
+# run with: uvicorn status_handler:app
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class ConnectivityEventDetail(BaseModel):
+    deviceStatus: str
+
+class Event(BaseModel):
+    eventType: str
+    eventTime: str
+    eventDetail: ConnectivityEventDetail
+
+class Notification(BaseModel):
+    eventSubscriptionId: str
+    event: Event
+
+@app.post("/notifications")
+def receive_notification(notification: Notification):
+    if notification.event.eventDetail.deviceStatus == "REACHABLE":
+        print("Device is available")
+    else if notification.event.eventDetail.deviceStatus == "UNREACHABLE":
+        print("Device is not available")
--- integration_tests/test_slice.py
+++ integration_tests/test_slice.py
@@ -72,6 +72,21 @@ def test_getting_a_slice(client):
     new_slice.delete()
 
 def test_get_after_deleting_all_slices_marks_them_as_deleted(client):
+    slice = client.slices.create(
+        name="slicemock27",
+        network_id=NetworkIdentifier(mcc="236", mnc="30"),
+        slice_info=SliceInfo(service_type="eMBB", differentiator="44eab5"),
+        area_of_service=AreaOfService(poligon=[Point(latitude=47.344, longitude=104.349), Point(latitude=35.344, longitude=76.619), Point(latitude=12.344, longitude=142.541), Point(latitude=19.43, longitude=103.53)]),
+        notification_url="https://notify.me/here",
+        notification_auth_token= "samplenotificationtoken",
+        slice_downlink_throughput=Throughput(guaranteed=0, maximum=0),
+        slice_uplink_throughput=Throughput(guaranteed=0, maximum=0),
+        device_downlink_throughput=Throughput(guaranteed=0, maximum=0),
+        device_uplink_throughput=Throughput(guaranteed=0, maximum=0),
+        max_devices=3,
+        max_data_connections=12
+    )
+
     for network_slice in client.slices.getAll():
         network_slice.delete()
 
@@ -97,7 +112,6 @@ def test_deactivating_and_deleting_a_slice(client):
 
     while slice.state == "PENDING":
         slice.refresh()
-        print(slice.state)
         time.sleep(5)
 
     assert slice.state == "AVAILABLE"
@@ -106,7 +120,6 @@ def test_deactivating_and_deleting_a_slice(client):
 
     while slice.state == "AVAILABLE":
         slice.refresh()
-        print(slice.state)
         time.sleep(5)
 
     assert slice.state == "OPERATING"
@@ -115,7 +128,6 @@ def test_deactivating_and_deleting_a_slice(client):
 
     while slice.state == "OPERATING":
         slice.refresh()
-        print(slice.state)
         time.sleep(5)
 
     assert slice.state == "AVAILABLE"
