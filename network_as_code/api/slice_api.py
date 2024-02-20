0170a0a7df98a7b5cc060bfa62b14ccbb2b27e51
Somogyi "Som-Som" Gábor <gabor.somogyi@nokia.com>
2024-02-19 19:31:16 -0800

    Type safe throughput conversion from model to API field.

--- network_as_code/api/slice_api.py
+++ network_as_code/api/slice_api.py
@@ -17,9 +17,22 @@ import os
 import pdb
 import httpx
 
-from typing import Optional
+from typing import Optional, Any
+from pydantic import BaseModel
 
 from ..errors import error_handler
+
+class Throughput(BaseModel):
+    """
+    A class representing the `Throughput` API field.
+
+    #### Public Attributes:
+            guaranteed (float): the guaranteed throughput in kbps
+            maximum (float): the maximum throughput in kbps
+    """
+
+    guaranteed: Optional[float]
+    maximum: Optional[float]
 
 
 class SliceAPI:
@@ -40,11 +53,11 @@ class SliceAPI:
         notification_url,
         name: Optional[str] = None,
         notification_auth_token: Optional[str] = None,
-        area_of_service: Optional[any] = None,
-        slice_downlink_throughput: Optional[any] = None,
-        slice_uplink_throughput: Optional[any] = None,
-        device_downlink_throughput: Optional[any] = None,
-        device_uplink_throughput: Optional[any] = None,
+        area_of_service: Optional[Any] = None,
+        slice_downlink_throughput: Optional[Throughput] = None,
+        slice_uplink_throughput: Optional[Throughput] = None,
+        device_downlink_throughput: Optional[Throughput] = None,
+        device_uplink_throughput: Optional[Throughput] = None,
         max_data_connections: Optional[int] = None,
         max_devices: Optional[int] = None,
     ):
@@ -147,7 +160,7 @@ class SliceAPI:
     def convert_slice_info_obj(self, sliceInfo):
         return {k: str(v) for k, v in dict(sliceInfo).items()}
 
-    def convert_throughput_obj(self, throughput):
+    def convert_throughput_obj(self, throughput: Throughput):
         return {k: float(v) for k, v in dict(throughput).items()}
 
 
